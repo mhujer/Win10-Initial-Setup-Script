c3dea0c990b58102a9788c219b4375496e808104
Martin Hujer <mail@martinhujer.cz>
2025-07-11 17:57:15 +0200

    tuning

--- Win11.psm1
+++ Win11.psm1
@@ -145,7 +145,8 @@ Function DisableActionCenter {
 	If (!(Test-Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer")) {
 		New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" | Out-Null
 	}
-	Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type DWord -Value 1
+	# breaks Calendar from Clock in Taskbar
+	# Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type DWord -Value 1
 	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -Type DWord -Value 0
 }
 
